27a39e50f5ff270875e07e1e651e3748d822fda0
sinchanah208-hub <sinchanah208@gmail.com>
2026-02-11 14:32:24 +0530

    Update style.css

--- style.css
+++ style.css
@@ -1,1 +1,38 @@
+body {
+  font-family: Arial;
+  background: #f4f6f9;
+  padding: 30px;
+}
+
+h1 {
+  margin-bottom: 20px;
+}
+
+select {
+  padding: 8px;
+  margin-bottom: 20px;
+}
+
+.cards {
+  display: flex;
+  gap: 20px;
+}
+
+.card {
+  background: white;
+  padding: 20px;
+  border-radius: 12px;
+  width: 220px;
+  box-shadow: 0 4px 8px rgba(0,0,0,0.05);
+}
+
+h3 {
+  font-size: 14px;
+  color: #666;
+}
+
+p {
+  font-size: 26px;
+  font-weight: bold;
+}
 
